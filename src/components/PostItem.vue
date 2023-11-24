faf2f601f4fa341cc0ea6b92833f3d473a923f3c
elisabeth <elisabeth.r2ni@gmail.com>
2023-11-24 18:16:53 +0200

    Show posts from vuex store

---
+++ src/components/PostItem.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  author_name: {
+    type: String,
+    required: true
+  },
+  created_at: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  image_path: {
+    type: String,
+    required: false
+  },
+  image_alt: {
+    type: String,
+    required: false
+  },
+})
+</script>
+
+<template>
+  <article class="post post--grey" id="{{ id }}">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <header class="post__header">
+      <img class="post__profile" src="loginpic.jpeg" alt="Profile Image">
+      <p class="post__date">{{ created_at }}</p>
+    </header>
+    <img class="post__image" :src=image_path :alt=image_alt>
+    <p> {{ text }} </p>
+    <img class="post__like" src="thumbs-up-solid.svg" alt="Like Image">
+  </article>
+</template>
+
+<style scoped>
+</style>
---
+++ src/store.js
@@ -0,0 +1,103 @@
+import { createStore } from 'vuex'
+
+export const store = createStore({
+  state: {
+    posts: [
+      {
+        id: 1,
+        author_name: 'José Marià del Coca',
+        author_email: 'jose55@example.com',
+        created_at: 'October 7, 2023',
+        text: 'Turkiye 2023',
+        image_path: 'assets/postitus.jpeg',
+        image_alt: 'Travel image'
+      },
+      {
+        id: 2,
+        author_name: 'José Marià del Coca',
+        author_email: 'jose55@example.com',
+        created_at: 'October 5, 2023',
+        text: 'Toomemägi is so beautiful. Yesterday me and my dog had so much fun running through piles of colorful leaves!',
+        image_path: null,
+        image_alt: null
+      },
+      {
+        id: 3,
+        author_name: 'José Marià del Coca',
+        author_email: 'jose55@example.com',
+        created_at: 'October 1, 2023',
+        text: "Girls and I spent a weekend in Milano. There was so much shopping and eating to do that we'll come back next month too.",
+        image_path: null,
+        image_alt: null
+      },
+      {
+        id: 4,
+        author_name: 'José Marià del Coca',
+        author_email: 'jose55@example.com',
+        created_at: 'September 25, 2023',
+        text: "I can't believe how blessed I am!! My neighbours surpised me with a pool party after I came back from long buissness trip.",
+        image_path: null,
+        image_alt: null
+      },
+      {
+        id: 5,
+        author_name: 'José Marià del Coca',
+        author_email: 'jose55@example.com',
+        created_at: 'September 19, 2023',
+        text: "I'm so proud of my little brother. He bought his first private jet all by himself.",
+        image_path: null,
+        image_alt: null
+      },
+      {
+        id: 6,
+        author_name: 'Rosalina Pacífica Castillo Rojas',
+        author_email: 'ro.rojas@example.com',
+        created_at: 'October 7, 2023',
+        text: "Just had the most incredible time at the FC Barcelona game! The energy at the stadium was absolutely electric, and the passion of the fans was off the charts. Witnessing the magic of Messi on the field and the teamwork that makes this club legendary was a dream come true. ⚽❤️🔵🔴 Can't wait for the next match! #FCBarcelona #FootballMagic #UnforgettableExperience",
+        image_path: 'assets/messi.jpeg',
+        image_alt: 'Football game'
+      },
+      {
+        id: 7,
+        author_name: 'Rosalina Pacífica Castillo Rojas',
+        author_email: 'ro.rojas@example.com',
+        created_at: 'October 7, 2023',
+        text: "Just experienced the enchanting Vienna Christmas Market, and it's pure holiday magic! The twinkling lights, festive decorations, and the scent of mulled wine and roasted chestnuts filled the air. And to top it off, the snowfall added that perfect touch of winter wonderland! ❄️🎄✨ #ViennaChristmas #MagicalMoments #SnowyDelight",
+        image_path: 'assets/christmas.jpeg',
+        image_alt: 'Christmas market'
+      },
+      {
+        id: 8,
+        author_name: 'Rosalina Pacífica Castillo Rojas',
+        author_email: 'ro.rojas@example.com',
+        created_at: 'October 7, 2023',
+        text: "Just had an eventful start to my Mexico trip! Unfortunately, my luggage went on its own adventure and got lost in transit. 😅 But thanks to my trusty Apple AirTag, I was able to track it down and reunite with my belongings. 🛄✈️ It's incredible how technology can come to the rescue when you least expect it! Grateful for a smooth recovery and ready to continue exploring this beautiful country. 🇲🇽💼 #TravelAdventure #AppleAirTag #LostAndFound",
+        image_path: 'assets/mexico.jpeg',
+        image_alt: 'Mexico'
+      },
+      {
+        id: 9,
+        author_name: 'Rosalina Pacífica Castillo Rojas',
+        author_email: 'ro.rojas@example.com',
+        created_at: 'October 7, 2023',
+        text: "🚀 Just secured my ticket to the ultimate destination – the MOON! 🌕 Thanks to my visionary outlook and a billion-dollar net worth, I've become one of the exclusive pioneers to book a space adventure with SpaceX. 💰💫 There's no stopping the limitless possibilities of private space travel, and I'm beyond excited to be at the forefront of this groundbreaking journey! 🌌✨ #SpaceXBillionaire #ToTheMoonAndBeyond #LivingTheDream",
+        image_path: null,
+        image_alt: null
+      },
+      {
+        id: 10,
+        author_name: 'Rosalina Pacífica Castillo Rojas',
+        author_email: 'ro.rojas@example.com',
+        created_at: 'October 7, 2023',
+        text: '🐧😍 So, today I had the most unexpected and adorable encounter – PENGUINS! 🐧❤️ I was in a remote, icy wilderness when these little waddlers popped out from the snow. Their cuteness factor is OFF THE CHARTS! 📸🏔️ #PenguinLove #WildlifeAdventures #AntarcticaMagic 🥰❄️',
+        image_path: null,
+        image_alt: null
+      }
+    ]
+  },
+  getters: {
+    allPosts(state) {
+      return state.posts
+    }
+  }
+})
