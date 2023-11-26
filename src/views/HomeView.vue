b3c33ebae866d5432aea2b16e8c857c3ff1674b3
brigittaambur <116078679+brigittaambur@users.noreply.github.com>
2023-11-26 23:36:17 +0200

    reset button size

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,7 +29,7 @@ export default{
         <PostItem :id=post.id :author_name=post.author_name :created_at=post.created_at :text=post.text :image_path=post.image_path :image_alt=post.image_alt :likes=post.likes>
         </PostItem>
       </div>
-    <button v-on:click="resetLikes">Reset Likes</button>
+    <button class="reset" v-on:click="resetLikes">Reset Likes</button>
     <Footer></Footer>
   </div>
 </template>
